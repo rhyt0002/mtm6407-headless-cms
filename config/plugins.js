aa4964531fd53d5f011051d9dedab22ade3734c4
Rhythm <rhyt0002@algonquinlive.com>
2024-12-07 14:19:16 -0500

    Headless cms

---
+++ config/plugins.js
@@ -0,0 +1,26 @@
+module.exports = ({ env }) => ({
+    'strapi-neon-tech-db-branches': {
+        enabled: true,
+        config: {
+          neonApiKey: env('NEON_API_KEY'),
+          neonProjectName: env('NEON_PROJECT_NAME'),
+          neonRole: env('NEON_ROLE'),
+          gitBranch: env('GIT_BRANCH'),
+        }
+      },
+      upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+      },
+});
